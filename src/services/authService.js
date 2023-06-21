0bd0b75b1c8a11ffd18a20ac2a2b6ad86b7a8ebf
Carlos Ramirez <ramirespinoza@gmail.com>
2023-06-20 20:06:16 -0600

    fixed votingcenter

--- src/services/authService.js
+++ src/services/authService.js
@@ -23,7 +23,7 @@ export const register = async (
   username,
   password,
   roleId,
-  centerVoterId
+  votingCenterId
 ) => {
   try {
     const { data } = await axios.post(`${appUrl}/auth/register`, {
@@ -32,7 +32,7 @@ export const register = async (
       username,
       password,
       roleId,
-      centerVoterId,
+      votingCenterId,
     });
     return data;
   } catch (error) {
